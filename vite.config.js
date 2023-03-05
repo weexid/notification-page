4e4a46ae0dc2e3c5b70f2a5001545525b65dcfeb
unknown <weexid@gmail.com>
2023-03-06 07:02:05 +0800

    update base pada vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/notification-page/',
+  base: 'https://github.com/weexid/notification-page/',
   plugins: [react()],
 })
