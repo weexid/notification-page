d4a27910750c87a03f838ba54dfee39fa242533a
I Wayan Darmadi <60530431+weexid@users.noreply.github.com>
2023-03-06 07:16:31 +0800

    test path public

--- src/data/notificationData.js
+++ src/data/notificationData.js
@@ -1,6 +1,6 @@
 const now = new Date();
 export const notifData = [
-    {profile: '/img/avatar-mark-webber.webp', nama: "Mark Webber", title: "reacted to your recent post", subject: "My first tournament today!", timestamp: new Date(now.getTime() - 1 * 60 * 1000).toString(), description: null, read: false},
+    {profile: './img/avatar-mark-webber.webp', nama: "Mark Webber", title: "reacted to your recent post", subject: "My first tournament today!", timestamp: new Date(now.getTime() - 1 * 60 * 1000).toString(), description: null, read: false},
     {profile: '/img/avatar-angela-gray.webp', nama: "Angela Gray", title: "followed you", subject: null, timestamp: new Date(now.getTime() - 300 * 1000).getTime(), description: null, read: false},
     {profile: '/img/avatar-jacob-thompson.webp', nama: "Jacob Thompson" , title: "has joined your group", subject: "Chess Club", timestamp: new Date(now.getTime() - 24 * 60 * 60 * 1000).getTime(), description: null, read: false},
     {profile: '/img/avatar-rizky-hasanuddin.webp', nama: "Rizky Hasanuddin" , title: "sent you a private message", subject: null, timestamp: new Date(now.getTime() - 120 * 60 * 60 * 1000).getTime(), description: "Hello, thanks for setting up the Chess Club. i've been a member for a few weeks now and i'm already having lots of fun and improving my game", read: true},
